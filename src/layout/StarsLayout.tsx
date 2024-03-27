719932233c68ababac323272114fa7f250dc460f
Karoline <karolrds00@gmail.com>
2024-03-27 13:17:25 -0400

    feat/add 404 page

--- src/layout/StarsLayout.tsx
+++ src/layout/StarsLayout.tsx
@@ -1,11 +1,11 @@
 import './index.css'
 const StarsLayout = ({ children }: { children: React.ReactNode }) => {
-    return (<main className="h-screen w-full overflow-hidden" id='star'>
+    return (<main className="min-h-screen w-full overflow-hidden" id='star'>
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
 
-        <section className='w-screen h-screen flex items-center justify-center'>
+        <section className='w-screen min-h-screen flex items-center justify-center'>
             {children}
         </section>
     </main>
--- src/pages/Notfound/Notfound.tsx
+++ src/pages/Notfound/Notfound.tsx
@@ -1,16 +1,19 @@
 import StarsLayout from "@/layout/StarsLayout";
+import { Link } from "react-router-dom";
 
 const Notfound = () => {
   return (
     <StarsLayout>
       <main className=" p-10 py-16 rounded-2xl">
         <div className="flex flex-col gap-6 ">
-          <p className="text-md md:text-lg lg:text-5xl uppercase font-extrabold text-white">
-            Não se perca na imensidão do espaço
+          <p className="text-2xl  lg:text-4xl xl:text-5xl uppercase text-center font-extrabold text-white">
+            Você se perdeu na imensidão do espaço
           </p>
-          <p className=" text-slate-950 p-2 text-center">
-            Volte para tela de início
+          <Link to={'/'}className="bg-white min-w-48 self-center rounded-md text-slate-950 p-2 text-center">
+          <p >
+            Volte para o início
           </p>
+          </Link>
         </div>
       </main>
     </StarsLayout>
